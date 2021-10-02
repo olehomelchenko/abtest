e39bbc88d68797e4d7b81a47311a15e0010fb7b4
Oleh <ptrvtch@protonmail.com>
2021-10-02 22:00:53 +0300

    add zscore calculation

--- processing.py
+++ processing.py
@@ -1,4 +1,5 @@
 from math import sqrt
+from scipy.stats import norm
 
 
 class Processor:
@@ -15,20 +16,22 @@ class Processor:
             lst[i] = [int(item) for item in l]
         return lst
 
-    def _add_std(self, variations):
-        for v in variations:
-            rate = v["cr"]
-            v["std"] = sqrt(rate * (1 - rate) / v["size"])
-
     def to_dict(self) -> dict:
         var_list = self.variations_list.copy()
-        cb = var_list.pop(0)
-        control_dict = {"size": cb[0], "conv": cb[1], "cr": cb[1] / cb[0]}
         variations = [
             {"size": item[0], "conv": item[1], "cr": item[1] / item[0]}
             for item in var_list
         ]
 
-        self._add_std(variations)
+        for v in variations:
+            rate = v["cr"]
+            v["std"] = sqrt(rate * (1 - rate) / v["size"])
+        control = variations[0]
+        for v in variations[1:]:
+            v["z"] = (v["cr"] - control["cr"]) / sqrt(
+                control["std"] ** 2 + v["std"] ** 2
+            )
+            v["p"] = norm().sf(v["z"])
 
-        return {"control": control_dict, "variations": variations}
+
+        return {"variations": variations}
