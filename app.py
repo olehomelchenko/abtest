ba97c8a5aba60060fc916bded722fc4eab9f8e12
Oleh Omelchenko <ptrvtch+github@protonmail.com>
2022-02-15 15:49:44 +0200

    add plotting functionality

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from processing import Processor
+import io
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+
 
 app = FastAPI()
 
@@ -14,3 +17,18 @@ def get_json(data: str):
     p = Processor(data)
     # p.parse_init_data()
     return p.variations_dict
+
+
+@app.get("/plot")
+def plot(data: str, linewidth="1", x="800", y="450", axis_off=False):
+    p = Processor(data)
+    x = int(x)
+    y = int(y)
+    linewidth = int(linewidth) or 1
+
+    fig = p.get_plot_figure(axis_off=axis_off, linewidth=linewidth, x=x, y=y)
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+
+    return Response(output.getvalue(), media_type="image/png")
+
--- processing.py
+++ processing.py
@@ -1,5 +1,15 @@
 from math import sqrt
+from pprint import pprint
+import numpy as np
 from scipy.stats import norm
+from matplotlib import ticker
+from matplotlib.pyplot import Figure
+
+
+@ticker.FuncFormatter
+def major_formatter(x, pos):
+    # return "[%.2f]" % x
+    return f"{x * 100:.1f}%"
 
 
 class Processor:
@@ -41,5 +51,86 @@ class Processor:
             )
             v["p"] = norm().sf(v["z"])
 
-
         return {"variations": variations}
+
+    def get_plot_figure(
+        self, dpi=96, x=800, y=450, axis_off=True, linewidth=1
+    ) -> Figure:
+        d = self.variations_dict["variations"]
+
+        # def create_norm_figure(testdata, dpi=96, x=800, y=450, axis_off=True, linewidth=1):
+        # set figure size based on pixels and dpi
+        fig = Figure(figsize=(x / dpi, y / dpi), dpi=dpi)
+
+        # add a single subplot to the figure
+        ax = fig.add_subplot(1, 1, 1)
+
+        if len(self.variations_dict) >= 2:
+            raise NotImplementedError
+
+        # get standard deviations for both experiments
+        std_a = d[0]["std"]
+        std_b = d[1]["std"]
+
+        # get borders for X axis, +-6 STD from rates
+        min_std = min(std_a, std_b)
+        max_std = max(std_a, std_b)
+
+        min_rate = min(d[0]["cr"], d[1]["cr"])
+        max_rate = max(d[0]["cr"], d[1]["cr"])
+
+        min_x = min_rate - 6 * min_std
+        max_x = max_rate + 6 * max_std
+
+        # generate array for X axis
+        click_rate = np.linspace(min_x, max_x, 300)
+
+        # generate Y values for both distributions
+        prob_a = norm(d[0]["cr"], std_a).pdf(click_rate)
+        prob_b = norm(d[1]["cr"], std_b).pdf(click_rate)
+
+        # plot both distributions on the subplot
+        ax.plot(
+            click_rate, prob_a, label="A", color="blue", alpha=0.5, linewidth=linewidth
+        )
+        ax.plot(
+            click_rate,
+            prob_b,
+            label="B",
+            color="orange",
+            alpha=0.5,
+            linewidth=linewidth,
+        )
+
+        # set X and Y labels
+        ax.set_xlabel("Conversion rate")
+        ax.set_ylabel("Probability")
+
+        # define top borders for vertical lines in the center of the "bell"
+        max_y_a = norm(d[0]["cr"], std_a).pdf(d[0]["cr"])
+        max_y_b = norm(d[1]["cr"], std_b).pdf(d[1]["cr"])
+
+        # plot vertical lines
+        ax.vlines(d[0]["cr"], ymin=0, ymax=max_y_a, color="blue", alpha=0.3)
+        ax.vlines(d[1]["cr"], ymin=0, ymax=max_y_b, color="orange", alpha=0.3)
+
+        # remove ticks on axes
+        ax.tick_params(axis="both", which="both", length=0)
+
+        # remove borders around the subplot
+        ax.spines["top"].set_visible(False)
+        ax.spines["right"].set_visible(False)
+        ax.spines["bottom"].set_visible(False)
+        ax.spines["left"].set_visible(False)
+
+        ax.xaxis.set_major_formatter(major_formatter)
+
+        fig.tight_layout()
+
+        if axis_off:
+            ax.axis("off")
+            fig.subplots_adjust(bottom=0, top=1, left=0, right=1)
+        else:
+            ax.legend(frameon=False)
+        return fig
+
